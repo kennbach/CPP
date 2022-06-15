7b02817db655b0537abcca855810d86e4b3cff0b
Kenn Bach <3580672+kennbach@users.noreply.github.com>
2022-06-15 13:14:23 -1000

    helloWorld

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, World!";
+
+  // uncomment to pause for input before closing app
+  // std::cin.get();
+
+  // return not required in main() for modern C++
+  // return 0;
+}
