c239176b03613a3d98f53cf73e25dd47305d7bcb
Kenn Bach <3580672+kennbach@users.noreply.github.com>
2022-06-16 09:35:47 -1000

    variables

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+
+int main()
+{
+
+  int count = 0;          /* signed -2b -> 2b */
+  unsigned int score = 0; /* unsigned 0 -> 4b */
+
+  char char1 = 'A'; /* char is just a number in memory*/
+  char char2 = 65; /* this char also will print as 'A' in cout*/
+
+  float float1 = 3.14; /* double, not a float */
+  float float2 = 3.14f; /* floats require trailing f or F */
+
+  double double1 = 2.0;
+
+  bool bool1 = 0; /* 1 byte , because 1 bit is not addressable */
+  bool bool2 = false;
+
+}
